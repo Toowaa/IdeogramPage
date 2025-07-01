d03b215c7aa28ea65158c930eb3bfe4629fa9050
Toowaa <brahanax@gmail.com>
2025-07-01 10:37:11 -0500

    Add NonImages page and create api.ts file

---
+++ app/make/page.tsx
@@ -0,0 +1,8 @@
+export default function NonImages() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-2xl font-bold">Non-Image Content</h1>
+      <p className="mt-4">This page does not contain any images.</p>
+    </div>
+  );
+}
